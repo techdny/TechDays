5731af8be01903504db7e4ccf3df561a6892a9d5
Sebastian Pravda <sebastian.pravda@gmail.com>
2023-11-18 15:52:35 +0100

    improve launch script

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,5 +1,3 @@
 git clone https://github.com/techdny/TechDays
 cd TechDays
-code . index.html styles.css
-chmod +x ./scripts/live-server
-./scripts/live-server .
+code . index.html styles.css && chmod +x ./scripts/live-server && ./scripts/live-server -p 3000 .
